bea56ae751b76165cb146c609cc2e7b98642cf03
Arash <aras72h@gmail.com>
2024-07-19 14:27:47 +0330

    Implement authorization

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-// middleware/authMiddleware.js
+// middlewares/authMiddleware.js
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -8,11 +8,25 @@ const authenticateToken = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        req.user = decoded; // This contains the userId from the token
         next();
     } catch (ex) {
         res.status(400).json({ error: 'Invalid token.' });
     }
 };
 
-module.exports = authenticateToken;
+const authorizeUser = (req, res, next) => {
+    const userId = req.user.userId; // Extracted from the token in authenticateToken
+    const requestedUserId = parseInt(req.params.id, 10); // ID from the route parameter
+
+    if (userId !== requestedUserId) {
+        return res.status(403).json({ error: 'Forbidden: You do not have access to this resource' });
+    }
+
+    next();
+};
+
+module.exports = {
+    authenticateToken,
+    authorizeUser
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { validateUserProfileUpdate } = require('../middlewares/validationMiddleware');
-const authenticateToken = require('../middlewares/authMiddleware');
+const { authenticateToken, authorizeUser } = require('../middlewares/authMiddleware');
 const userController = require('../controllers/userController');
 
 // User profile routes
-router.get('/:id', authenticateToken, userController.getUserProfile); // GET /users/:id
-router.put('/:id', [authenticateToken, validateUserProfileUpdate()], userController.updateUserProfile); // PUT /users/:id
-router.delete('/:id', authenticateToken, userController.deleteUserAccount); // DELETE /users/:id
+router.get('/:id', [authenticateToken, authorizeUser], userController.getUserProfile); // GET /users/:id
+router.put('/:id', [authenticateToken, authorizeUser, validateUserProfileUpdate()], userController.updateUserProfile); // PUT /users/:id
+router.delete('/:id', [authenticateToken, authorizeUser], userController.deleteUserAccount); // DELETE /users/:id
 
 module.exports = router;
